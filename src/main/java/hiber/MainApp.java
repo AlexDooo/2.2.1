adaadd0c53145da4d8f2e9e8cf4ab3a431a5952e
AlexDooo <krash_pvp@mail.ru>
2021-02-23 22:09:51 +0300

    Commit

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -18,9 +18,9 @@ public class MainApp {
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
         UserService userService = context.getBean(UserService.class);
-        //userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Mazda", 3345)));
-        //userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Opel", 100001)));
-        //userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Ford", 999)));
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Mazda", 3345)));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Opel", 100001)));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Ford", 999)));
 
 
         List<User> users = userService.listUsers();
